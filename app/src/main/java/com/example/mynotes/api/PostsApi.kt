ee50ca34b06297d34541de9e6988b4f7a2070a25
Luis Piura <luisf.piura@hotmail.com>
2023-11-07 13:00:53 -0600

    Add interception for HTTP requests

--- app/src/main/java/com/example/mynotes/api/PostsApi.kt
+++ app/src/main/java/com/example/mynotes/api/PostsApi.kt
@@ -1,5 +1,9 @@
 package com.example.mynotes.api
 
+import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -13,10 +17,45 @@ interface PostsApi {
 
 object PostsService {
     fun retrofitProvider(): PostsApi {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor())
+            .build()
         return Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(PostsApi::class.java)
     }
+}
+
+class AuthInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val authorizeRequest = originalRequest.newBuilder()
+            .header("Authorization", "Bearer dummy_token")
+            .build()
+        /*
+        If a refresh token is needed, you would need to inject the auth service and
+        get the new token inside the `runBlocking` block
+        val refreshedToken = runBlocking {
+                // apiService is a retrofit service created with Retrofit.Builder()
+            val response = apiService.refreshAccessToken()
+            response.accessToken
+         }
+
+            if (refreshedToken != null) {
+                // Create a new request with the refreshed access token
+                val newRequest = originalRequest.newBuilder()
+                    .header("Authorization", "Bearer $refreshedToken")
+                    .build()
+
+                // Retry the request with the new access token
+                return chain.proceed(newRequest)
+            }
+         */
+        Log.d("REQUEST_INTERCEPTED", "${authorizeRequest.header("Authorization")}")
+        return chain.proceed(authorizeRequest)
+    }
+
 }
